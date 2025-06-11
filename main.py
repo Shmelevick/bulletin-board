f6949b8e6ea2a594dff91d9ca4c949def72797ff
Mac <yaroslav1992r@gmail.com>
2025-06-11 18:03:12 +0300

    Add bboard/index and Bb model

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from django-4-dronov-vladimir!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ samplesite/bboard/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
+
+
+def index(request):
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
+
+
+
+
