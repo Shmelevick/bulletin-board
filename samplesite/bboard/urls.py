b21e0f57ec08cba70f2bd0dcf01a6e44038e9bfb
Mac <yaroslav1992r@gmail.com>
2025-06-13 12:01:45 +0300

    Add rubric

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, rubric_bbs, BbCreateView
 
 urlpatterns = [
-    path('', index),
+    path('add/', BbCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', rubric_bbs, name='rubric_bbs'),
+    path('', index, name='index'),
 ]
